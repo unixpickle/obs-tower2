96730874ddb96a202096ea0988454bbed87950e2
Alex Nichol <unixpickle@gmail.com>
2019-03-07 15:14:31 -0500

    drafted PPO script

---
+++ obs_tower2/scripts/run_ppo.py
@@ -0,0 +1,30 @@
+import torch
+
+from obs_tower2.constants import IMAGE_SIZE, IMAGE_DEPTH
+from obs_tower2.model import ACModel
+from obs_tower2.ppo import PPO
+from obs_tower2.roller import Roller
+from obs_tower2.util import create_batched_env
+
+NUM_ENVS = 8
+HORIZON = 512
+BATCH_SIZE = NUM_ENVS * HORIZON / 8
+LR = 1e-4
+ITERS = 24
+ENTROPY_REG = 0.001
+GAE_LAM = 0.95
+GAE_GAMMA = 0.9975
+
+
+def main():
+    env = create_batched_env(NUM_ENVS)
+    model = ACModel(54, IMAGE_SIZE, IMAGE_DEPTH)
+    model.load_state_dict(torch.load('save.pkl'))
+    model.to(torch.device('cuda'))
+    roller = Roller(env, model, HORIZON)
+    ppo = PPO(model, gamma=GAE_GAMMA, lam=GAE_LAM, lr=LR, ent_reg=ENTROPY_REG)
+    ppo.outer_loop(roller, num_steps=ITERS, batch_size=BATCH_SIZE)
+
+
+if __name__ == '__main__':
+    main()
--- obs_tower2/util.py
+++ obs_tower2/util.py
@@ -1,8 +1,37 @@
 from PIL import Image
+from anyrl.envs import batched_gym_env
+from anyrl.envs.wrappers import FrameStackEnv
+import gym
 import numpy as np
+import os
 
 
 def big_obs(obs, info):
     res = (info['brain_info'].visual_observations[0][0, :, :, :] * 255).astype(np.uint8)
     res[:20] = np.array(Image.fromarray(obs).resize((168, 168)))[:20]
     return res
+
+
+def create_batched_env(num_envs):
+    return batched_gym_env([lambda i=i: create_single_env(i) for i in range(num_envs)])
+
+
+def create_single_env(idx, clear=True):
+    from obstacle_tower_env import ObstacleTowerEnv
+    env = ObstacleTowerEnv(os.environ['OBS_TOWER_PATH'], worker_id=idx)
+    env = FrameStackEnv(env)
+    if clear:
+        env = ClearInfoEnv(env)
+    return env
+
+
+class ClearInfoEnv(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+    def step(self, action):
+        obs, rew, done, _ = self.env.step(action)
+        return obs, rew, done, {}
