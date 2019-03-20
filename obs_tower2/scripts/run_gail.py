39ef32718f01df557dda2c0d73e083d3fb8ebecb
Alex Nichol <unixpickle@gmail.com>
2019-03-20 14:12:14 -0400

    regular entropy regularization

--- obs_tower2/scripts/run_gail.py
+++ obs_tower2/scripts/run_gail.py
@@ -5,7 +5,7 @@ import torch
 from obs_tower2.constants import IMAGE_SIZE, IMAGE_DEPTH, NUM_ACTIONS
 from obs_tower2.gail import GAIL
 from obs_tower2.model import ACModel, DiscriminatorModel
-from obs_tower2.prierarchy import Prierarchy
+from obs_tower2.ppo import PPO
 from obs_tower2.recording import load_data
 from obs_tower2.util import LogRoller, create_batched_env
 
@@ -14,7 +14,7 @@ HORIZON = 512
 BATCH_SIZE = NUM_ENVS * HORIZON // 8
 LR = 3e-5
 ITERS = 24
-PRIOR_REG = 0.003
+ENT_REG = 0.01
 GAE_LAM = 0.95
 GAE_GAMMA = 0.9975
 REWARD_SCALE = 0.01
@@ -25,21 +25,17 @@ GAIL_NUM_ENVS = (HORIZON * NUM_ENVS) // GAIL_HORIZON
 def main():
     env = create_batched_env(NUM_ENVS, augment=True)
     model = ACModel(NUM_ACTIONS, IMAGE_SIZE, IMAGE_DEPTH)
-    prior = ACModel(NUM_ACTIONS, IMAGE_SIZE, IMAGE_DEPTH)
     discriminator = DiscriminatorModel(IMAGE_SIZE, IMAGE_DEPTH)
     if os.path.exists('save.pkl'):
         model.load_state_dict(torch.load('save.pkl'))
     if os.path.exists('save_disc.pkl'):
         discriminator.load_state_dict(torch.load('save_disc.pkl'))
-    if os.path.exists('save_prior.pkl'):
-        prior.load_state_dict(torch.load('save_prior.pkl'))
     model.to(torch.device('cuda'))
-    prior.to(torch.device('cuda'))
     discriminator.to(torch.device('cuda'))
     train, test = load_data(augment=True)
     recordings = train + test
     roller = LogRoller(env, model, HORIZON)
-    ppo = Prierarchy(prior, model, gamma=GAE_GAMMA, lam=GAE_LAM, lr=LR, ent_reg=PRIOR_REG)
+    ppo = PPO(model, gamma=GAE_GAMMA, lam=GAE_LAM, lr=LR, ent_reg=ENT_REG)
     gail = GAIL(discriminator, lr=LR)
     gail.outer_loop(ppo,
                     roller,
