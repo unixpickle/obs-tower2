e12fe76166d048d8d2b70ea6672d2f7c00730d3a
Alex Nichol <unixpickle@gmail.com>
2019-03-26 17:44:45 -0400

    plot training logs

---
+++ obs_tower2/scripts/plot_logs.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+import matplotlib.pyplot as plt
+
+KEYS = ['entropy', 'floor']
+
+
+def main():
+    paths = sys.argv[1:]
+    logs = [read_log(path) for path in paths]
+    names = [os.path.basename(path) for path in paths]
+    for key in KEYS:
+        plt.figure()
+        for log in logs:
+            values = log[key]
+            plt.plot([i * 4096 / 1e6 for i, _ in enumerate(values)], values)
+        plt.legend(names)
+        plt.xlabel('timesteps (1e6)')
+        plt.ylabel(key)
+        plt.savefig(key + '.png')
+
+
+def read_log(log_path):
+    with open(log_path, 'r') as in_file:
+        lines = [l for l in in_file.readlines() if 'step' in l or 'floor=' in l]
+    mean_floor = None
+    entropies = []
+    floors = []
+    for l in lines:
+        if 'floor=' in l:
+            floor_num = float(l.split('floor=')[-1])
+            if mean_floor is None:
+                mean_floor = floor_num
+            else:
+                mean_floor += 0.01 * (floor_num - mean_floor)
+        elif 'step' in l:
+            entropies.append(float(l.split('entropy=')[1].split(' ')[0]))
+            floors.append(mean_floor if mean_floor is not None else 0.0)
+    return {'entropy': entropies, 'floor': floors}
+
+
+if __name__ == '__main__':
+    main()
