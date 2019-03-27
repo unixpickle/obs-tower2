583f5d9019d237e407aa315fdd24463298c89ff1
Alex Nichol <unixpickle@gmail.com>
2019-03-27 16:57:36 -0400

    augment in clone

--- obs_tower2/scripts/run_clone.py
+++ obs_tower2/scripts/run_clone.py
@@ -23,7 +23,7 @@ def main():
         model.load_state_dict(torch.load('save_clone.pkl'))
     model.to(torch.device('cuda'))
     optimizer = optim.Adam(model.parameters(), lr=LR)
-    train, test = load_data()
+    train, test = load_data(augment=True)
     for i in itertools.count():
         train_rollout = recording_rollout(train, BATCH, HORIZON)
         test_rollout = recording_rollout(test, BATCH, HORIZON)
