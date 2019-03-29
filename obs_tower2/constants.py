1045b5543d01ec7d56e2c81d3203099d348f8bb8
Alex Nichol <unixpickle@gmail.com>
2019-03-29 11:21:04 -0400

    log dataset stats

--- obs_tower2/constants.py
+++ obs_tower2/constants.py
@@ -3,6 +3,8 @@ FRAME_STACK = 2
 IMAGE_DEPTH = FRAME_STACK * 3
 HUMAN_ACTIONS = (0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33)
 NUM_ACTIONS = len(HUMAN_ACTIONS)
-NUM_LABELS = 11
+LABELS = ('closed door', 'locked door', 'boxed door', 'open door', 'key', 'box', 'hurtle', 'orb',
+          'goal', 'box target', 'box undo')
+NUM_LABELS = len(LABELS)
 STATE_SIZE = NUM_ACTIONS + 2 + NUM_LABELS
 STATE_STACK = 50
---
+++ obs_tower2/labeler/stats.py
@@ -0,0 +1,18 @@
+from obs_tower2.constants import LABELS, NUM_LABELS
+from obs_tower2.labels import load_all_labeled_images
+
+
+def main():
+    images = load_all_labeled_images()
+    counts = [0] * NUM_LABELS
+    for img in images:
+        for i, label in enumerate(img.pack_labels()):
+            if label:
+                counts[i] += 1
+    print('total of %d images' % len(images))
+    for name, count in zip(LABELS, counts):
+        print('%s: %d (%d%%)' % (name, count, round(count / len(images) * 100)))
+
+
+if __name__ == '__main__':
+    main()
