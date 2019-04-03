a641345d41bdf33c84d9bf98022a40723346f477
Alex Nichol <unixpickle@gmail.com>
2019-04-03 14:31:27 -0400

    tail model in evaluation

--- obs_tower2/scripts/eval_floors.py
+++ obs_tower2/scripts/eval_floors.py
@@ -11,20 +11,22 @@ from obs_tower2.states import StateEnv
 def main():
     env = StateEnv(create_single_env(random.randrange(20, 25)))
     model = ACModel()
-    state = torch.load('save.pkl', map_location='cpu')
-    model.load_state_dict(state)
-    model.to(torch.device('cuda'))
+    tail_model = ACModel()
+    model.load_state_dict(torch.load('save.pkl', map_location='cpu'))
+    tail_model.load_state_dict(torch.load('save_tail.pkl', map_location='cpu'))
+    tail_model.to(torch.device('cuda'))
     for seed in range(100):
         env.seed(seed)
         for _ in range(5):
-            print('%d,%d' % (seed, run_episode(env, model)))
+            print('%d,%d' % (seed, run_episode(env, model, tail_model)))
 
 
-def run_episode(env, model):
+def run_episode(env, model, tail_model):
     state, obs = env.reset()
     floor = 0
     while True:
-        output = model.step(np.array([state]), np.array([obs]))
+        m = (model if floor < 10 else tail_model)
+        output = m.step(np.array([state]), np.array([obs]))
         (state, obs), rew, done, info = env.step(output['actions'][0])
         if rew == 1.0:
             floor += 1
