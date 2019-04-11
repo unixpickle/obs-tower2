6991e68880e8f56f08cd96aae8b16460a3209f1c
Alex Nichol <unixpickle@gmail.com>
2019-04-11 09:23:00 -0400

    new hang setup

--- obs_tower2/scripts/catch_hang.py
+++ obs_tower2/scripts/catch_hang.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 import random
 
@@ -11,29 +12,31 @@ from obs_tower2.util import create_single_env
 
 
 def main():
-    env = StateEnv(create_single_env(random.randrange(15, 20), clear=False))
-    try:
-        model = ACModel()
-        model.load_state_dict(torch.load('save_tail.pkl', map_location='cpu'))
-        model.to(torch.device('cuda'))
-        while True:
-            seed = random.randrange(100)
-            floor = random.randrange(10, 15)
-            actions = []
-            env.unwrapped.seed(seed)
-            env.unwrapped.floor(floor)
-            state, obs = env.reset()
-            while True:
-                output = model.step(np.array([state]), np.array([obs]))
-                action = output['actions'][0]
-                actions.append(HUMAN_ACTIONS[action])
-                with open('hang.json', 'w+') as out_file:
-                    json.dump({'actions': actions, 'seed': seed, 'floor': floor}, out_file)
-                (state, obs), rew, done, info = env.step(action)
-                if done or rew == 1.0:
-                    break
-    finally:
-        env.close()
+    model = ACModel()
+    model.load_state_dict(torch.load('save_tail.pkl', map_location='cpu'))
+    model.to(torch.device('cuda'))
+    for i in itertools.count():
+        env = StateEnv(create_single_env(i % 16, clear=False))
+        try:
+            seeds = [random.randrange(100), 58, random.randrange(100)]
+            floors = [random.randrange(10, 15), 10, random.randrange(10, 15)]
+            act_seqs = [[], [], []]
+            for seed, floor, actions in zip(seeds, floors, act_seqs):
+                env.unwrapped.seed(seed)
+                env.unwrapped.floor(floor)
+                state, obs = env.reset()
+                while True:
+                    output = model.step(np.array([state]), np.array([obs]))
+                    action = output['actions'][0]
+                    actions.append(HUMAN_ACTIONS[action])
+                    with open('hang.json', 'w+') as out_file:
+                        json.dump({'seeds': seeds, 'floors': floors, 'actions': act_seqs}, out_file)
+                    (state, obs), rew, done, info = env.step(action)
+                    if done or rew == 1.0:
+                        print('ended episode: rew=%f' % rew)
+                        break
+        finally:
+            env.close()
 
 
 if __name__ == '__main__':
