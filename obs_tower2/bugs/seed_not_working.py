a8c127bff854a0b4f82d646d6e5b910914656c40
Alex Nichol <unixpickle@gmail.com>
2019-05-19 09:22:31 -0400

    catch bigger bug

--- obs_tower2/bugs/seed_not_working.py
+++ obs_tower2/bugs/seed_not_working.py
@@ -1,15 +1,15 @@
 import os
+import random
 
 from obstacle_tower_env import ObstacleTowerEnv
 
 counter = {}
-for i in range(0, 25):
-    env = ObstacleTowerEnv(os.environ['OBS_TOWER_PATH'], worker_id=i)
-    env.seed(25)
+env = ObstacleTowerEnv(os.environ['OBS_TOWER_PATH'], worker_id=2)
+while True:
+    env.seed(random.randrange(100))
     env.reset()
     for _ in range(50):
         obs, _, _, _ = env.step(0)
     key = str(obs.flatten().tolist())
     counter[key] = True
     print('got %d start states' % len(counter))
-    env.close()
