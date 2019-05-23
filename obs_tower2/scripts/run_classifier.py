4584ee9ac0397d70b2b2ba0a0355270e7ee1d308
Alex Nichol <unixpickle@gmail.com>
2019-05-23 10:37:29 -0400

    draft mixmatch

--- obs_tower2/scripts/run_classifier.py
+++ obs_tower2/scripts/run_classifier.py
@@ -8,11 +8,16 @@ import torch.nn as nn
 import torch.optim as optim
 
 from obs_tower2.labels import load_labeled_images
+from obs_tower2.recording import load_data
 from obs_tower2.model import StateClassifier
 from obs_tower2.util import Augmentation, atomic_save, mirror_obs
 
 LR = 1e-4
 BATCH = 128
+NUM_AUGMENTATIONS = 2
+UNLABELED_WEIGHT = 1
+MIXUP_ALPHA = 0.75
+TEMPERATURE = 0.5
 
 
 def main():
@@ -22,18 +27,62 @@ def main():
     model.to(torch.device('cuda'))
     optimizer = optim.Adam(model.parameters(), lr=LR)
     train, test = load_labeled_images()
+    recordings, _ = load_data()
     for i in itertools.count():
         test_loss = classification_loss(model, test).item()
-        train_loss = classification_loss(model, train)
-        print('step %d: test=%f train=%f' % (i, test_loss, train_loss.item()))
+        mm_loss = mixmatch_loss(model,
+                                *labeled_data(model, train),
+                                *unlabeled_data(model, recordings))
+        print('step %d: test=%f mixmatch=%f' % (i, test_loss, mm_loss.item()))
         optimizer.zero_grad()
-        train_loss.backward()
+        mm_loss.backward()
         optimizer.step()
         if not i % 100:
             atomic_save(model.state_dict(), 'save_classifier.pkl')
 
 
 def classification_loss(model, dataset):
+    image_tensor, label_tensor = labeled_data(model, dataset)
+    logits = model(image_tensor)
+    loss = nn.BCEWithLogitsLoss()
+    return loss(logits, label_tensor)
+
+
+def mixmatch_loss(model, real_images, real_labels, other_images, other_labels):
+    real_images, real_labels, other_images, other_labels = mixmatch(real_images, real_labels,
+                                                                    other_images, other_labels)
+    model_out = model(torch.cat([real_images, other_images]))
+    real_out = model_out[:real_images.shape[0]]
+    other_out = model_out[real_images.shape[0]:]
+
+    bce = nn.BCEWithLogitsLoss()
+    real_loss = bce(real_out, real_labels)
+    other_loss = torch.mean(torch.pow(other_out - other_labels, 2))
+    return real_loss + UNLABELED_WEIGHT * other_loss
+
+
+def mixmatch(real_images, real_labels, other_images, other_labels):
+    all_images = torch.cat([real_images, other_images])
+    all_labels = torch.cat([real_labels, other_labels])
+    indices = list(range(all_images.shape[0]))
+    random.shuffle(indices)
+    all_images, all_labels = mixup(all_images, all_labels,
+                                   all_images[indices], all_labels[indices])
+    return (all_images[:real_images.shape[0]], all_labels[:real_labels.shape[0]],
+            all_images[real_images.shape[0]:], all_labels[real_labels.shape[0]:])
+
+
+def mixup(real_images, real_labels, other_images, other_labels):
+    probs = []
+    for _ in range(real_images.shape[0]):
+        p = np.random.beta(MIXUP_ALPHA, MIXUP_ALPHA)
+        probs.append(min(p, 1 - p))
+    prob_tensor = torch.from_numpy(np.array(probs)).to(real_images.device)
+    return (real_images + prob_tensor.view(-1, 1, 1, 1) * (other_images - real_images),
+            real_labels + prob_tensor.view(-1, 1, 1, 1) * (other_labels - real_labels))
+
+
+def labeled_data(model, dataset):
     images = []
     labels = []
     for _ in range(BATCH):
@@ -46,12 +95,40 @@ def classification_loss(model, dataset):
         labels.append(sample.pack_labels())
     images = np.array(images, dtype=np.uint8)
     labels = np.array(labels, dtype=np.float32)
+    image_tensor = model_tensor(model, images)
+    label_tensor = model_tensor(model, labels)
+    return image_tensor, label_tensor
+
+
+def unlabeled_data(model, recordings):
+    images = []
+    for _ in range(BATCH):
+        for _ in range(NUM_AUGMENTATIONS):
+            aug = Augmentation()
+            rec = random.choice(recordings)
+            img = rec.load_frame(random.randrange(rec.num_steps))
+            img = np.array(aug.apply(img))
+            if random.random() < 0.5:
+                img = mirror_obs(img)
+            images.append(img)
+    image_tensor = model_tensor(model, np.array(images, dtype=np.uint8))
+    preds = torch.sigmoid(model(image_tensor)).detach()
+    preds = preds.view(BATCH, NUM_AUGMENTATIONS)
+    mixed = torch.mean(preds, dim=1, keepdim=True)
+    sharpened = sharpen_predictions(mixed)
+    broadcasted = (sharpened + torch.zeros_like(preds)).view(-1)
+    return image_tensor, broadcasted
+
+
+def sharpen_predictions(preds):
+    pow1 = torch.pow(preds, 1 / TEMPERATURE)
+    pow2 = torch.pow(1 - preds, 1 / TEMPERATURE)
+    return pow1 / (pow1 + pow2)
+
+
+def model_tensor(model, nparray):
     device = next(model.parameters()).device
-    image_tensor = torch.from_numpy(images).to(device)
-    label_tensor = torch.from_numpy(labels).to(device)
-    logits = model(image_tensor)
-    loss = nn.BCEWithLogitsLoss()
-    return loss(logits, label_tensor)
+    return torch.from_numpy(nparray).to(device)
 
 
 if __name__ == '__main__':
